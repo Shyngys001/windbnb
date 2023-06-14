b55f66a5b0714c88ec410df370df8fb0bd4170c7
ShingsiHairolla <93879495+Shyngys001@users.noreply.github.com>
2023-06-15 01:58:58 +0600

    Add files via upload

--- index.5f489231.js
+++ index.5f489231.js
@@ -1,17 +1,1 @@
-function toggleColors() {
-    const themeImage = document.querySelector('.theme');
-    if (themeImage.src.includes('/dark.b04c358c.png')) {
-      document.documentElement.style.setProperty("--white", "white");
-      document.documentElement.style.setProperty("--black", "black");
-      document.documentElement.style.setProperty("--red", "red");
-      document.documentElement.style.setProperty("--grey", "$color-grey");
-      themeImage.src = '/light.758fd574.png';
-
-    } else if (themeImage.src.includes('/light.758fd574.png')) {
-      themeImage.src = '/dark.b04c358c.png';
-      document.documentElement.style.setProperty("--white", "black");
-      document.documentElement.style.setProperty("--black", "white");
-      document.documentElement.style.setProperty("--red", "yellow");
-      document.documentElement.style.setProperty("--grey", "white");
-    }
-  }
+console.log("dummy file");
